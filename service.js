bdfafd316ac6d57a8644daa0f33503886a40fe25
Andres De Castro <af.decastro879@gmail.com>
2016-02-19 16:49:23 -0500

    Allow Cross Origin

--- service.js
+++ service.js
@@ -2,16 +2,26 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser"),
     methodOverride = require("method-override"),
-    mongoose       = require('mongoose');
+    mongoose       = require('mongoose'),
+    cors           = require('cors');
 
-  mongoose.connect('mongodb://localhost:27017/db', function(err, res) {
+  mongoose.connect('mongodb://ec2-54-172-2-45.compute-1.amazonaws.com:27017/kml_db', function(err, res) {
     if(err) throw err;
     console.log('Connected to Database');
   });
 // Middlewares
+//app.use(cors);
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(allowCrossDomain);
 
 // Import Models and Controllers
 var modelCity = require('./models/city')(app, mongoose);
@@ -33,4 +43,4 @@ app.use('/', pets);
 // Start Server
 app.listen(3000, function(){
 	console.log("Server runing on http://localhost:3000");
-})
+});
