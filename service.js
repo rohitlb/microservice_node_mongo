7a40bac5fcb0bbd7f0fc547f688e0c1328d49841
Andres De Castro <af.decastro879@gmail.com>
2016-02-28 18:27:01 -0500

    REgistro al apiGateway

--- service.js
+++ service.js
@@ -3,9 +3,9 @@ var express        = require("express"),
     bodyParser     = require("body-parser"),
     methodOverride = require("method-override"),
     mongoose       = require('mongoose'),
-    cors           = require('cors');
+    requestify     = require("requestify");
 
-  mongoose.connect('mongodb://ec2-54-172-2-45.compute-1.amazonaws.com:27017/kml_db', function(err, res) {
+  mongoose.connect('mongodb://ec2-54-164-141-121.compute-1.amazonaws.com:27017/kml_db', function(err, res) {
     if(err) throw err;
     console.log('Connected to Database');
   });
@@ -23,6 +23,24 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(allowCrossDomain);
 
+//Register app to api gateway
+const apiGatewayUrl = "http://localhost:3002/apps";
+requestify.request(apiGatewayUrl, {
+    method: "POST",
+    body: {
+        "appName": "petsLocation",
+        "hostName": "localhost",
+        "port": 3000,
+        "service": "/pet/location",
+        "method": "GET"
+    },
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(function (response) {
+    console.log("Service registered successfully");
+});
+
 // Import Models and Controllers
 var modelCity = require('./models/city')(app, mongoose);
 var modelPet = require('./models/pet')(app, mongoose);
@@ -36,7 +54,7 @@ pets.get('/hello', function(req, res) {
 });
 
 pets.route('/pet/location/:pet')
-	.get(CityCtrl.findClosePlaces)
+	.get(CityCtrl.findClosePlaces);
 
 app.use('/', pets);
 
